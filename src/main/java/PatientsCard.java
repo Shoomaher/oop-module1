af6bb4a8b3f07e34608716cfddada4454b753539
Mi <mihail.shoomaher@gmail.com>
2018-10-21 18:09:56 +0300

    added docs

--- src/main/java/PatientsCard.java
+++ src/main/java/PatientsCard.java
@@ -4,12 +4,21 @@ public class PatientsCard {
     private String address;
     private int insuranceId;
 
+    /**
+     * @param firstName
+     * @param lastName
+     */
     public PatientsCard(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.insuranceId = 0;
     }
 
+    /**
+     * @param firstName
+     * @param lastName
+     * @param adress
+     */
     public PatientsCard(String firstName, String lastName, String adress) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -17,6 +26,12 @@ public class PatientsCard {
         this.insuranceId = 0;
     }
 
+    /**
+     * @param firstName
+     * @param lastName
+     * @param adress
+     * @param insuranceId
+     */
     public PatientsCard(String firstName, String lastName, String adress, int insuranceId) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -24,34 +39,59 @@ public class PatientsCard {
         this.insuranceId = insuranceId;
     }
 
+    /**
+     * @return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     *
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * @return address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * @param address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * @return insuranceId
+     */
     public int getInsuranceId() {
         return insuranceId;
     }
 
+    /**
+     * @param insuranceId
+     */
     public void setInsuranceId(int insuranceId) {
         this.insuranceId = insuranceId;
     }
