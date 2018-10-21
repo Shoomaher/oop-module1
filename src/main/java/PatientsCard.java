dd8149858f5554af3d0158fc5f3b0b931686b33c
Mi <mihail.shoomaher@gmail.com>
2018-10-21 15:41:39 +0300

    implemented 1st task (PatientsCard class)

--- src/main/java/PatientsCard.java
+++ src/main/java/PatientsCard.java
@@ -3,4 +3,56 @@ public class PatientsCard {
     private String lastName;
     private String adress;
     private int insuranceId;
+
+    public PatientsCard(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.insuranceId = 0;
+    }
+
+    public PatientsCard(String firstName, String lastName, String adress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.insuranceId = 0;
+    }
+
+    public PatientsCard(String firstName, String lastName, String adress, int insuranceId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.insuranceId = insuranceId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getInsuranceId() {
+        return insuranceId;
+    }
+
+    public void setInsuranceId(int insuranceId) {
+        this.insuranceId = insuranceId;
+    }
 }
