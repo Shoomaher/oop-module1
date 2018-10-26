1f9ee7e14cf9b3d62d86f9098c1f802f6de8e72e
Mi <mihail.shoomaher@gmail.com>
2018-10-26 03:21:29 +0300

    changed comments

--- src/main/java/PatientsCard.java
+++ src/main/java/PatientsCard.java
@@ -4,56 +4,48 @@
 public interface PatientsCard {
     /**
      * Gets first name.
-     *
      * @return the first name
      */
     String getFirstName();
 
     /**
      * Sets first name.
-     *
      * @param name the name
      */
     void setFirstName(String name);
 
     /**
      * Gets last name.
-     *
      * @return the last name
      */
     String getLastName();
 
     /**
      * Sets last name.
-     *
      * @param name the name
      */
     void setLastName(String name);
 
     /**
      * Gets address.
-     *
      * @return the address
      */
     String getAddress();
 
     /**
      * Sets address.
-     *
      * @param address the address
      */
     void setAddress(String address);
 
     /**
      * Gets insurance policy.
-     *
      * @return the insurance policy
      */
     MedicalInsurancePolicy getInsurancePolicy();
 
     /**
      * Sets insurance policy.
-     *
      * @param insurancePolicy the insurance policy
      */
     void setInsurancePolicy(MedicalInsurancePolicy insurancePolicy);
