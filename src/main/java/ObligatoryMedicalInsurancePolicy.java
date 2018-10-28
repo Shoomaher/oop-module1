ed5437c46342387a8f6ad0b4467dda92d2cfdb4d
Mi <mihail.shoomaher@gmail.com>
2018-10-28 19:55:17 +0200

    Implemented constructors from super class

--- src/main/java/ObligatoryMedicalInsurancePolicy.java
+++ src/main/java/ObligatoryMedicalInsurancePolicy.java
@@ -1,4 +1,11 @@
 public class ObligatoryMedicalInsurancePolicy extends MedicalInsurancePolicy {
+    public ObligatoryMedicalInsurancePolicy() {
+    }
+
+    public ObligatoryMedicalInsurancePolicy(int policyId, String insuranceCompanyName) {
+        super(policyId, insuranceCompanyName);
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Obligatory medical insurance policy with id: ");
